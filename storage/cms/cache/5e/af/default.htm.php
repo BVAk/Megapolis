3323e2722f2ed1658f54a2209e071e82b77b9292
BVAk <bekkviktoria55@gmail.com>
2020-05-26 20:44:24 +0300

    first commit

---
+++ storage/cms/cache/5e/af/default.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5ecc18dc66a02900184221_fd50f420d78a758456a5d657473657abClass extends Cms\Classes\LayoutCode
+{
+
+}
---
+++ storage/framework/classes.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'backend\\Controllers\\users' => 'modules\\backend\\controllers\\users.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'backend\\Controllers\\preferences' => 'modules\\backend\\controllers\\preferences.php',
+  'cms\\Controllers\\themes' => 'modules\\cms\\controllers\\themes.php',
+);
